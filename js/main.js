c089db637931392e9eb5d42bf6a30a44358667c0
enki14 <jyakarisuto@gmail.com>
2023-02-13 00:24:56 +0900

    20230213 commit

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
 const mainSwiper = new Swiper('.l-award__swiper', {
     // Optional parameters
     loop: true,
-    slidesPerView: 3,
     loopedSlides: 8,
+    slidesPerView: 'auto',
     // spaceBetween: 50,
+    watchSlidesVisibility: true,
     breakpoints: {
+        1380: {
+            slidesPerView: 3.5
+        },
+        1080: {
+            slidesPerView: 3
+        },
+        970: {
+            slidesPerView: 2.5
+        },
         831: {
-            slidesPerView: 1.5
+            slidesPerView: 2
         },
         421: {
             slidesPerView: 1.25
@@ -14,7 +24,7 @@ const mainSwiper = new Swiper('.l-award__swiper', {
     },
     slidesPerGroup: 1,
     centeredSlides : true,
-    effect: 'slide',
+    // effect: 'slide',
 
     // speed: 1000,
     autoplay: {
@@ -33,11 +43,5 @@ const mainSwiper = new Swiper('.l-award__swiper', {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
-
-    // And if we need scrollbar
-    // scrollbar: {
-    //     el: '.swiper-scrollbar',
-    //     hide: true
-    // },
 
 });
